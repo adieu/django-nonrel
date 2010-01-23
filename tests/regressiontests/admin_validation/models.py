adieu/django-nonrel

commit 2569e9a0f07f1627ff84979f57805ad717f627cd

Refs #12203 -- Improved error handling for the case where a user manually specifies an m2m field with an explicit through field. Thanks to dgouldin for the report, and Ramiro Morales for the patch.
git-svn-id: http://code.djangoproject.com/svn/django/trunk@11737 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--HG--
extra : convert_revision : a7b54dfb0cbbc9169f13afb2ff556dfe941ed3b6

django/contrib/admin/validation.py
@@ -149,7 +149,7 @@ def validate(cls, model):
             validate_inline(inline, cls, model)
 
 def validate_inline(cls, parent, parent_model):
-    
+
     # model is already verified to exist and be a Model
     if cls.fk_name: # default value is None
         f = get_field(cls, cls.model, cls.model._meta, 'fk_name', cls.fk_name)
@@ -196,6 +196,9 @@ def validate_base(cls, model):
         check_isseq(cls, 'fields', cls.fields)
         for field in cls.fields:
             check_formfield(cls, model, opts, 'fields', field)
+            f = get_field(cls, model, opts, 'fields', field)
+            if isinstance(f, models.ManyToManyField) and not f.rel.through._meta.auto_created:
+                raise ImproperlyConfigured("'%s.fields' can't include the ManyToManyField field '%s' because '%s' manually specifies a 'through' model." % (cls.__name__, field, field))
         if cls.fieldsets:
             raise ImproperlyConfigured('Both fieldsets and fields are specified in %s.' % cls.__name__)
         if len(cls.fields) > len(set(cls.fields)):

tests/regressiontests/admin_validation/models.py
@@ -108,7 +108,18 @@ Exception: <class 'regressiontests.admin_validation.models.TwoAlbumFKAndAnE'> ha
 
 >>> validate_inline(TwoAlbumFKAndAnEInline, None, Album)
 
-# Regression test for #12203 -- If the explicitly provided through model
+# Regression test for #12203 - Fail more gracefully when a M2M field that
+# specifies the 'through' option is included in the 'fields' ModelAdmin option.
+
+>>> class BookAdmin(admin.ModelAdmin):
+...     fields = ['authors']
+
+>>> validate(BookAdmin, Book)
+Traceback (most recent call last):
+    ...
+ImproperlyConfigured: 'BookAdmin.fields' can't include the ManyToManyField field 'authors' because 'authors' manually specifies a 'through' model.
+
+# Regression test for #12209 -- If the explicitly provided through model
 # is specified as a string, the admin should still be able use
 # Model.m2m_field.through
 

commit 022796f0a4cdb7b7e6108f68b0396153f0b27c0f

ModelAdmin.fields wasn't able to refer to fields only on a custom form
Regressed in r11737 which used get_field instead of opts.get_field and ignoring
fields not found.

git-svn-id: http://code.djangoproject.com/svn/django/trunk@12279 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--HG--
extra : convert_revision : 2c95607abc05fd9317cf0e4d4e8c1f6ffbce08ba

django/contrib/admin/validation.py
@@ -211,7 +211,12 @@ def validate_base(cls, model):
                 # validation of such things.
                 continue
             check_formfield(cls, model, opts, 'fields', field)
-            f = get_field(cls, model, opts, 'fields', field)
+            try:
+                f = opts.get_field(field)
+            except models.FieldDoesNotExist:
+                # If we can't find a field on the model that matches,
+                # it could be an extra field on the form.
+                continue
             if isinstance(f, models.ManyToManyField) and not f.rel.through._meta.auto_created:
                 raise ImproperlyConfigured("'%s.fields' can't include the ManyToManyField "
                     "field '%s' because '%s' manually specifies "

tests/regressiontests/admin_validation/models.py
@@ -215,4 +215,18 @@ ImproperlyConfigured: 'FieldsetBookAdmin.fieldsets[1][1]['fields']' can't includ
 # the validation will fail.
 >>> validate(BookAdmin, Book)
 
+# Regression for ensuring ModelAdmin.fields can contain non-model fields
+# that broke with r11737
+
+>>> class SongForm(forms.ModelForm):
+...     extra_data = forms.CharField()
+...     class Meta:
+...         model = Song
+
+>>> class FieldsOnFormOnlyAdmin(admin.ModelAdmin):
+...     form = SongForm
+...     fields = ['title', 'extra_data']
+
+>>> validate(FieldsOnFormOnlyAdmin, Song)
+
 """}