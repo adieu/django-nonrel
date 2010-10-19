adieu/django-nonrel

commit 84f56640fd7ea8dfc3c62b1bb1970fef1ae3365d

Fixed #12991 -- Added unittest2 support. Thanks to PaulM for the draft patch, and to Luke, Karen, Justin, Alex, Łukasz Rekucki, and Chuck Harmston for their help testing and reviewing the final patch.
git-svn-id: http://code.djangoproject.com/svn/django/trunk@14139 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--HG--
extra : convert_revision : 7ce0ad747cacb8f5b90f00b1efbed3d6fd2dd011

django/db/backends/__init__.py
@@ -20,6 +20,7 @@ class BaseDatabaseWrapper(local):
         self.queries = []
         self.settings_dict = settings_dict
         self.alias = alias
+        self.vendor = 'unknown'
 
     def __eq__(self, other):
         return self.settings_dict == other.settings_dict
@@ -87,15 +88,104 @@ class BaseDatabaseFeatures(object):
     needs_datetime_string_cast = True
     empty_fetchmany_value = []
     update_can_self_select = True
+
+    # Does the backend distinguish between '' and None?
     interprets_empty_strings_as_nulls = False
+
     can_use_chunked_reads = True
     can_return_id_from_insert = False
     uses_autocommit = False
     uses_savepoints = False
+
     # If True, don't use integer foreign keys referring to, e.g., positive
     # integer primary keys.
     related_fields_match_type = False
     allow_sliced_subqueries = True
+
+    # Does the default test database allow multiple connections?
+    # Usually an indication that the test database is in-memory
+    test_db_allows_multiple_connections = True
+
+    # Can an object be saved without an explicit primary key?
+    supports_unspecified_pk = False
+
+    # Can a fixture contain forward references? i.e., are
+    # FK constraints checked at the end of transaction, or
+    # at the end of each save operation?
+    supports_forward_references = True
+
+    # Does a dirty transaction need to be rolled back
+    # before the cursor can be used again?
+    requires_rollback_on_dirty_transaction = False
+
+    # Does the backend allow very long model names without error?
+    supports_long_model_names = True
+
+    # Is there a REAL datatype in addition to floats/doubles?
+    has_real_datatype = False
+    supports_subqueries_in_group_by = True
+    supports_bitwise_or = True
+
+    # Do time/datetime fields have microsecond precision?
+    supports_microsecond_precision = True
+
+    # Does the __regex lookup support backreferencing and grouping?
+    supports_regex_backreferencing = True
+
+    # Can date/datetime lookups be performed using a string?
+    supports_date_lookup_using_string = True
+
+    # Can datetimes with timezones be used?
+    supports_timezones = True
+
+    # When performing a GROUP BY, is an ORDER BY NULL required
+    # to remove any ordering?
+    requires_explicit_null_ordering_when_grouping = False
+
+    # Is there a 1000 item limit on query parameters?
+    supports_1000_query_paramters = True
+
+    # Can an object have a primary key of 0? MySQL says No.
+    allows_primary_key_0 = True
+
+    # Features that need to be confirmed at runtime
+    # Cache whether the confirmation has been performed.
+    _confirmed = False
+    supports_transactions = None
+    supports_stddev = None
+
+    def __init__(self, connection):
+        self.connection = connection
+
+    def confirm(self):
+        "Perform manual checks of any database features that might vary between installs"
+        self._confirmed = True
+        self.supports_transactions = self._supports_transactions()
+        self.supports_stddev = self._supports_stddev()
+
+    def _supports_transactions(self):
+        "Confirm support for transactions"
+        cursor = self.connection.cursor()
+        cursor.execute('CREATE TABLE ROLLBACK_TEST (X INT)')
+        self.connection._commit()
+        cursor.execute('INSERT INTO ROLLBACK_TEST (X) VALUES (8)')
+        self.connection._rollback()
+        cursor.execute('SELECT COUNT(X) FROM ROLLBACK_TEST')
+        count, = cursor.fetchone()
+        cursor.execute('DROP TABLE ROLLBACK_TEST')
+        self.connection._commit()
+        return count == 0
+
+    def _supports_stddev(self):
+        "Confirm support for STDDEV and related stats functions"
+        class StdDevPop(object):
+            sql_function = 'STDDEV_POP'
+
+        try:
+            self.connection.ops.check_aggregate_support(StdDevPop())
+        except DatabaseError:
+            self.supports_stddev = False
+
 
 class BaseDatabaseOperations(object):
     """

django/db/backends/sqlite3/base.py
@@ -60,6 +60,27 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     # setting ensures we always read result sets fully into memory all in one
     # go.
     can_use_chunked_reads = False
+    test_db_allows_multiple_connections = False
+    supports_unspecified_pk = True
+    supports_1000_query_paramters = False
+
+    def _supports_stddev(self):
+        """Confirm support for STDDEV and related stats functions
+
+        SQLite supports STDDEV as an extension package; so
+        connection.ops.check_aggregate_support() can't unilaterally
+        rule out support for STDDEV. We need to manually check
+        whether the call works.
+        """
+        cursor = self.connection.cursor()
+        cursor.execute('CREATE TABLE STDDEV_TEST (X INT)')
+        try:
+            cursor.execute('SELECT STDDEV(*) FROM STDDEV_TEST')
+            has_support = True
+        except utils.DatabaseError:
+            has_support = False
+        cursor.execute('DROP TABLE STDDEV_TEST')
+        return has_support
 
 class DatabaseOperations(BaseDatabaseOperations):
     def date_extract_sql(self, lookup_type, field_name):
@@ -129,7 +150,7 @@ class DatabaseOperations(BaseDatabaseOperations):
         return value
 
 class DatabaseWrapper(BaseDatabaseWrapper):
-
+    vendor = 'sqlite'
     # SQLite requires LIKE statements to include an ESCAPE clause if the value
     # being escaped has a percent or underscore in it.
     # See http://www.sqlite.org/lang_expr.html for an explanation.
@@ -153,7 +174,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
     def __init__(self, *args, **kwargs):
         super(DatabaseWrapper, self).__init__(*args, **kwargs)
 
-        self.features = DatabaseFeatures()
+        self.features = DatabaseFeatures(self)
         self.ops = DatabaseOperations()
         self.client = DatabaseClient(self)
         self.creation = DatabaseCreation(self)

tests/regressiontests/queries/models.py
@@ -8,7 +8,7 @@ import sys
 import threading
 
 from django.conf import settings
-from django.db import models, DEFAULT_DB_ALIAS
+from django.db import models, DEFAULT_DB_ALIAS, connection
 from django.db.models import Count
 from django.db.models.query import Q, ITER_CHUNK_SIZE, EmptyQuerySet
 
@@ -1307,13 +1307,13 @@ FieldError: Infinite loop caused by ordering.
 
 
 # In Oracle, we expect a null CharField to return u'' instead of None.
-if settings.DATABASES[DEFAULT_DB_ALIAS]['ENGINE'] == "django.db.backends.oracle":
+if connection.features.interprets_empty_strings_as_nulls:
     __test__["API_TESTS"] = __test__["API_TESTS"].replace("<NONE_OR_EMPTY_UNICODE>", "u''")
 else:
     __test__["API_TESTS"] = __test__["API_TESTS"].replace("<NONE_OR_EMPTY_UNICODE>", "None")
 
 
-if settings.DATABASES[DEFAULT_DB_ALIAS]['ENGINE'] == "django.db.backends.mysql":
+if connection.features.requires_explicit_null_ordering_when_grouping:
     __test__["API_TESTS"] += """
 When grouping without specifying ordering, we add an explicit "ORDER BY NULL"
 portion in MySQL to prevent unnecessary sorting.
@@ -1342,7 +1342,7 @@ Using an empty generator expression as the rvalue for an "__in" lookup is legal
 
 # Sqlite 3 does not support passing in more than 1000 parameters except by
 # changing a parameter at compilation time.
-if settings.DATABASES[DEFAULT_DB_ALIAS]['ENGINE'] != "django.db.backends.sqlite3":
+if connection.features.supports_1000_query_paramters:
     __test__["API_TESTS"] += """
 Bug #14244: Test that the "in" lookup works with lists of 1000 items or more.
 >>> Number.objects.all().delete()

django/contrib/admindocs/tests/__init__.py
@@ -1,7 +1,8 @@
-import unittest
-import fields
 from django.contrib.admindocs import views
 from django.db.models import fields as builtin_fields
+from django.utils import unittest
+
+import fields
 
 
 class TestFieldType(unittest.TestCase):

django/contrib/gis/gdal/tests/__init__.py
@@ -2,7 +2,7 @@
 Module for executing all of the GDAL tests.  None
 of these tests require the use of the database.
 """
-from unittest import TestSuite, TextTestRunner
+from django.utils.unittest import TestSuite, TextTestRunner
 
 # Importing the GDAL test modules.
 import test_driver, test_ds, test_envelope, test_geom, test_srs

django/contrib/gis/geos/tests/__init__.py
@@ -1,7 +1,7 @@
 """
 GEOS Testing module.
 """
-from unittest import TestSuite, TextTestRunner
+from django.utils.unittest import TestSuite, TextTestRunner
 import test_geos, test_io, test_geos_mutation, test_mutable_list
 
 test_suites = [

django/contrib/gis/tests/__init__.py
@@ -1,9 +1,10 @@
 import sys
-import unittest
 
 from django.conf import settings
 from django.db.models import get_app
 from django.test.simple import build_suite, DjangoTestSuiteRunner
+from django.utils import unittest
+
 
 def run_tests(*args, **kwargs):
     from django.test.simple import run_tests as base_run_tests

django/db/backends/dummy/base.py
@@ -42,7 +42,7 @@ class DatabaseWrapper(object):
     _rollback = ignore
 
     def __init__(self, settings_dict, alias, *args, **kwargs):
-        self.features = BaseDatabaseFeatures()
+        self.features = BaseDatabaseFeatures(self)
         self.ops = DatabaseOperations()
         self.client = DatabaseClient(self)
         self.creation = BaseDatabaseCreation(self)

commit 24a0ee419f227edc10ae9138bd73fbb2079cb31e

Fixed a small typo introduced in r14139.
--HG--
extra : convert_revision : svn%3Abcc190cf-cafb-0310-a4f2-bffc1f526a37/django/trunk%4014272

django/db/backends/__init__.py
@@ -145,7 +145,7 @@ class BaseDatabaseFeatures(object):
     requires_explicit_null_ordering_when_grouping = False
 
     # Is there a 1000 item limit on query parameters?
-    supports_1000_query_paramters = True
+    supports_1000_query_parameters = True
 
     # Can an object have a primary key of 0? MySQL says No.
     allows_primary_key_0 = True

django/db/backends/sqlite3/base.py
@@ -62,7 +62,7 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     can_use_chunked_reads = False
     test_db_allows_multiple_connections = False
     supports_unspecified_pk = True
-    supports_1000_query_paramters = False
+    supports_1000_query_parameters = False
 
     def _supports_stddev(self):
         """Confirm support for STDDEV and related stats functions

tests/regressiontests/queries/models.py
@@ -1342,7 +1342,7 @@ Using an empty generator expression as the rvalue for an "__in" lookup is legal
 
 # Sqlite 3 does not support passing in more than 1000 parameters except by
 # changing a parameter at compilation time.
-if connection.features.supports_1000_query_paramters:
+if connection.features.supports_1000_query_parameters:
     __test__["API_TESTS"] += """
 Bug #14244: Test that the "in" lookup works with lists of 1000 items or more.
 >>> Number.objects.all().delete()