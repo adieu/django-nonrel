adieu/django-nonrel

commit ba424ccfdad5e5dddf7dc3c4a5438aeef99f3582

Fixed #14533 -- Make django signals more thread-safe. Thanks milosu for the patch!
git-svn-id: http://code.djangoproject.com/svn/django/trunk@14662 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--HG--
extra : convert_revision : f793de865612626272a90ef805e2adde9035667d

django/dispatch/dispatcher.py
@@ -1,4 +1,5 @@
 import weakref
+import threading
 
 from django.dispatch import saferef
 
@@ -30,6 +31,7 @@ class Signal(object):
         if providing_args is None:
             providing_args = []
         self.providing_args = set(providing_args)
+        self.lock = threading.Lock()
 
     def connect(self, receiver, sender=None, weak=True, dispatch_uid=None):
         """
@@ -97,11 +99,15 @@ class Signal(object):
         if weak:
             receiver = saferef.safeRef(receiver, onDelete=self._remove_receiver)
 
-        for r_key, _ in self.receivers:
-            if r_key == lookup_key:
-                break
-        else:
-            self.receivers.append((lookup_key, receiver))
+        try:
+            self.lock.acquire()
+            for r_key, _ in self.receivers:
+                if r_key == lookup_key:
+                    break
+            else:
+                self.receivers.append((lookup_key, receiver))
+        finally:
+            self.lock.release()
 
     def disconnect(self, receiver=None, sender=None, weak=True, dispatch_uid=None):
         """
@@ -130,11 +136,15 @@ class Signal(object):
         else:
             lookup_key = (_make_id(receiver), _make_id(sender))
         
-        for index in xrange(len(self.receivers)):
-            (r_key, _) = self.receivers[index]
-            if r_key == lookup_key:
-                del self.receivers[index]
-                break
+        try:
+            self.lock.acquire()
+            for index in xrange(len(self.receivers)):
+                (r_key, _) = self.receivers[index]
+                if r_key == lookup_key:
+                    del self.receivers[index]
+                    break
+        finally:
+            self.lock.release()
 
     def send(self, sender, **named):
         """
@@ -227,14 +237,21 @@ class Signal(object):
         Remove dead receivers from connections.
         """
 
-        to_remove = []
-        for key, connected_receiver in self.receivers:
-            if connected_receiver == receiver:
-                to_remove.append(key)
-        for key in to_remove:
-            for idx, (r_key, _) in enumerate(self.receivers):
-                if r_key == key:
-                    del self.receivers[idx]
+        try:
+            self.lock.acquire()
+            to_remove = []
+            for key, connected_receiver in self.receivers:
+                if connected_receiver == receiver:
+                    to_remove.append(key)
+            for key in to_remove:
+                last_idx = len(self.receivers) - 1
+                # enumerate in reverse order so that indexes are valid even
+                # after we delete some items
+                for idx, (r_key, _) in enumerate(reversed(self.receivers)):
+                    if r_key == key:
+                        del self.receivers[last_idx-idx]
+        finally:
+            self.lock.release()
 
 
 def receiver(signal, **kwargs):

commit 32071a51d8ff037f0e1b1b25c16db49d2a8e6d30

Preventing problems possibly introduced by r14662
acquire a lock before entering a try block that ends with the lock's
release. Thanks for the catch Alex.

git-svn-id: http://code.djangoproject.com/svn/django/trunk@14668 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--HG--
extra : convert_revision : 9f3cd99b1d9b457ad479ff56a24ee28a9d49a7b9

django/dispatch/dispatcher.py
@@ -99,8 +99,8 @@ class Signal(object):
         if weak:
             receiver = saferef.safeRef(receiver, onDelete=self._remove_receiver)
 
+        self.lock.acquire()
         try:
-            self.lock.acquire()
             for r_key, _ in self.receivers:
                 if r_key == lookup_key:
                     break
@@ -136,8 +136,8 @@ class Signal(object):
         else:
             lookup_key = (_make_id(receiver), _make_id(sender))
         
+        self.lock.acquire()
         try:
-            self.lock.acquire()
             for index in xrange(len(self.receivers)):
                 (r_key, _) = self.receivers[index]
                 if r_key == lookup_key:
@@ -237,8 +237,8 @@ class Signal(object):
         Remove dead receivers from connections.
         """
 
+        self.lock.acquire()
         try:
-            self.lock.acquire()
             to_remove = []
             for key, connected_receiver in self.receivers:
                 if connected_receiver == receiver: